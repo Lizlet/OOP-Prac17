a4aad4f4a2c51b87463f89c1b19411e6ac9e72ec
Sondre <songro93@gmail.com>
2017-03-10 05:56:10 +0100

    Finished of anything related to uploading

--- OOP-Prac17/Form1.vb
+++ OOP-Prac17/Form1.vb
@@ -1,8 +1,10 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim tables = FileReader.ReadFile
+        DataGridView1.DataSource = tables.Item1
+        DataGridView2.DataSource = tables.Item2
         DBConn.DBUpload.Upload(tables.Item1, tables.Item2)
 
-        DataGridView1.DataSource = tables.Item2
+
     End Sub
 End Class
