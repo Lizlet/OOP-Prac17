9bebb3d94af07eaacba10924a0b671bd5e947813
Sondre <songro93@gmail.com>
2017-03-10 06:47:07 +0100

    Continued the code for DBSearch
    opened the donnec,tion, closed and dispoed it at the end, and a template sql statement.

--- OOP-Prac17/DBConn/DBSearch.vb
+++ OOP-Prac17/DBConn/DBSearch.vb
@@ -2,5 +2,22 @@
 Namespace DBConn
     Public Class DBSearch
         Private Shared connString As String = $"Server={My.Resources.server}; Database={My.Resources.dbn}; Uid={My.Resources.uid}; Pwd={My.Resources.pwd};"
+        Private Shared connection As New MySqlConnection(connString)
+
+        Public Shared Function Search(locationName As String) As DataTable
+            Dim table As New DataTable
+
+            Try
+                connection.Open()
+                Dim sql As String = "select postNumber as ""post numbers in OSLO"" from post_numbers pn, post_locations pl where pn.locationID = pl.locationID and pl.locationName = ""OSLO"""
+            Catch ex As Exception
+                Debug.WriteLine($"something went wrong: {ex.Message}")
+            Finally
+                connection.Close()
+                connection.Dispose()
+            End Try
+
+            Return table
+        End Function
     End Class
 End Namespace
