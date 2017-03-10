d2faf868e48b50bd25ea6d7e31c79aadc55e860c
Sondre <songro93@gmail.com>
2017-03-10 05:48:46 +0100

    Changed the name of DBConn.vb to DBUpload.vb and made DBConn a namespace.

---
+++ OOP-Prac17/DBConn/DBUpload.vb
@@ -0,0 +1,42 @@
+﻿Imports MySql.Data.MySqlClient
+Namespace DBConn
+    Public Class DBUpload
+        Private Shared server As String = "mysql.stud.iie.ntnu.no"
+        Private Shared dbn As String = "sondg"
+        Private Shared uid As String = "sondg"
+        Private Shared pwd As String = "7ppasexr"
+        Private Shared connString As String = $"Server={server}; Database={dbn}; Uid={uid}; Pwd={pwd};"
+        Private Shared connection As New MySqlConnection(connString)
+
+        Public Shared Sub Upload(postLocations As DataTable, postNumbers As DataTable)
+            Try
+                connection.Open()
+                PrepDatabase()
+                uploadTable(postLocations, "post_locations", False)
+                uploadTable(postNumbers, "post_numbers", True)
+            Catch ex As MySqlException
+                Debug.WriteLine($"something went wrong: {ex.Message}")
+            End Try
+        End Sub
+
+        Private Shared Sub uploadTable(table As DataTable, tName As String, firstFieldIsAString As Boolean)
+            Dim cmd As New MySqlCommand()
+            Dim sql As String = $"INSERT INTO `{tName}`(`{table.Columns(0).ColumnName}`, `{table.Columns(1).ColumnName}`) VALUES "
+
+            For i = 0 To table.Rows.Count - 1
+                sql += $"(@col1{i}, @col2{i}),"
+                cmd.Parameters.AddWithValue($"@col1{i}", table.Rows(i)(table.Columns(0).ColumnName))
+                cmd.Parameters.AddWithValue($"@col2{i}", table.Rows(i)(table.Columns(1).ColumnName))
+            Next
+
+            cmd.CommandText = sql.Substring(0, sql.Length - 1)
+            cmd.Connection = connection
+            cmd.ExecuteNonQuery()
+        End Sub
+
+        Private Shared Sub PrepDatabase()
+            Dim cmd As New MySqlCommand(My.Resources.createTables, connection)
+            cmd.ExecuteNonQuery()
+        End Sub
+    End Class
+End Namespace
--- OOP-Prac17/Form1.vb
+++ OOP-Prac17/Form1.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim tables = FileReader.ReadFile
-        DBConn.Upload(tables.Item1, tables.Item2)
+        DBConn.DBUpload.Upload(tables.Item1, tables.Item2)
 
         DataGridView1.DataSource = tables.Item2
     End Sub
