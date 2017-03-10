08d54bf8ab0252fb5cc9c0733e3ec0df33c600c6
Sondre <songro93@gmail.com>
2017-03-10 03:02:14 +0100

    Tentatively completed the code for reading the data from file, with comments, etc.

--- OOP-Prac17/FileReader/FileReader.vb
+++ OOP-Prac17/FileReader/FileReader.vb
@@ -1,4 +1,11 @@
-﻿Public Class FileReader
+﻿''' <summary>
+'''     the .txt file was added to the project as a resource, meaning I can do the reading bit a lot simpler
+'''     
+'''     I decided to use the hashcode of the location name as ID, saving me a lot of annoyance having to deal with finding the id
+'''     of already stored records in a datatable, I am sure I could do it, butI certainly cant be bothered.
+'''     Unless it turns out I have overlapping ids, but I doubt it. the probability with a samplesize this miniscule is abysmal
+''' </summary>
+Public Class FileReader
     Public Shared Function ReadFile() As Tuple(Of DataTable, DataTable)
         Dim postLocations As New DataTable
         Dim postNumbers As New DataTable
@@ -9,21 +16,22 @@
         postNumbers.Columns.Add("locationID")
 
         Dim x As String = My.Resources.Postnummerregister_ansi
-        Dim index As Integer = 0
         For Each line In x.Split(Environment.NewLine)
-            Dim lineSplit = line.Split(vbTab)
-            Dim plRow As DataRow = postLocations.NewRow
+            Dim lineSplit = line.Split(vbTab) 'lineSplit(0) = post number and lineSplit(1) = post location
             Dim pnRow As DataRow = postNumbers.NewRow
 
-            plRow("locationID") = index
-            plRow("locationName") = lineSplit(0)
-            pnRow("postNumber") = lineSplit(1)
-            pnRow("locationID") = index
+            ' to ensure the same location is not added twice.
+            Dim rows = postLocations.Select(String.Format("locationName = '{0}'", lineSplit(1)))
+            If rows.Length = 0 Then
+                Dim plRow As DataRow = postLocations.NewRow
+                plRow("locationID") = lineSplit(1).GetHashCode
+                plRow("locationName") = lineSplit(1)
+                postLocations.Rows.Add(plRow)
+            End If
 
-            postLocations.Rows.Add(plRow)
+            pnRow("postNumber") = lineSplit(0)
+            pnRow("locationID") = lineSplit(1).GetHashCode
             postNumbers.Rows.Add(pnRow)
-
-            index += 1
         Next
 
         Return New Tuple(Of DataTable, DataTable)(postLocations, postNumbers)
