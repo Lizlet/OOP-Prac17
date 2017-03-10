481fc0248d566ea7abe048462bfdcb044d4d6c38
Sondre <songro93@gmail.com>
2017-03-10 02:30:55 +0100

    Wrote the filereader code until it returns two datatables.
    currently it just reads every record there is into the datatable as is with an index as id. the intention forwards is to shrink it down to only on row per location in the postLocations table.

--- OOP-Prac17/FileReader/FileReader.vb
+++ OOP-Prac17/FileReader/FileReader.vb
@@ -1,3 +1,31 @@
 ﻿Public Class FileReader
+    Public Shared Function ReadFile() As Tuple(Of DataTable, DataTable)
+        Dim postLocations As New DataTable
+        Dim postNumbers As New DataTable
 
+        postLocations.Columns.Add("locationID")
+        postLocations.Columns.Add("locationName")
+        postNumbers.Columns.Add("postNumber")
+        postNumbers.Columns.Add("locationID")
+
+        Dim x As String = My.Resources.Postnummerregister_ansi
+        Dim index As Integer = 0
+        For Each line In x.Split(Environment.NewLine)
+            Dim lineSplit = line.Split(vbTab)
+            Dim plRow As DataRow = postLocations.NewRow
+            Dim pnRow As DataRow = postNumbers.NewRow
+
+            plRow("locationID") = index
+            plRow("locationName") = lineSplit(0)
+            pnRow("postNumber") = lineSplit(1)
+            pnRow("locationID") = index
+
+            postLocations.Rows.Add(plRow)
+            postNumbers.Rows.Add(pnRow)
+
+            index += 1
+        Next
+
+        Return New Tuple(Of DataTable, DataTable)(postLocations, postNumbers)
+    End Function
 End Class
